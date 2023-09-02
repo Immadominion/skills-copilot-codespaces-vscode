d79d5d739d94cb8bbe4674b1055ee1e9ae13d982
Nwakanma Dominion <118551746+Immadominion@users.noreply.github.com>
2023-09-02 07:38:50 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,20 @@
+// Create web server
+// Import the 'http' module
+const http = require('http');
+
+// Define the port on which the server will listen
+const port = 3000;
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Set the response header with a status code and content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Write the response content
+  res.end('Hello, World!\n');
+});
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+});
